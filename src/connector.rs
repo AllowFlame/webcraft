3e9d58b75f074536a49a0304447f1e4fd78e019f
AllowFlame <closelogin@hanmail.net>
2020-07-18 22:14:32 +0900

    bunch requesting test

--- src/connector.rs
+++ src/connector.rs
@@ -1,4 +1,41 @@
-use hyper::Client;
+use hyper::{Client, Uri};
+use hyper_tls::HttpsConnector;
+
+pub struct Connector {
+    uris: Vec<Uri>,
+}
+
+impl Connector {
+    pub fn new() -> Connector {
+        Connector { uris: Vec::new() }
+    }
+
+    pub fn push_uri(&mut self, target_uri: Uri) {
+        (&mut self.uris).push(target_uri)
+    }
+
+    #[tokio::main]
+    pub async fn connect(self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let https = HttpsConnector::new();
+        let client = Client::builder().build::<_, hyper::Body>(https);
+
+        let mut futs = Vec::new();
+        for uri in self.uris {
+            let fut = async {
+                let resp = client.get(uri).await?;
+                println!("res : {}", resp.status());
+                hyper::body::to_bytes(resp.into_body()).await
+            };
+            futs.push(fut);
+        }
+
+        for fut in futs {
+            let _ = fut.await;
+        }
+
+        Ok(())
+    }
+}
 
 #[tokio::main]
 pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -17,22 +54,38 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 #[tokio::main]
 pub async fn run1() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Client::new();
+    // let client = Arc::new(Client::new());
 
-    let uri = "http://naver.com".parse()?;
-    let resp = client.get(uri).await?;
+    let mut uris = Vec::new();
+    uris.push("https://naver.com");
+    uris.push("https://daum.net");
+    uris.push("https://google.com");
 
-    println!("2 Response: {}", resp.status());
+    for uri_str in uris {
+        // let client = client.clone();
+        let uri = uri_str.parse()?;
 
-    let uri = "http://daum.net".parse()?;
-    let resp = client.get(uri).await?;
+        tokio::spawn(async move {
+            let client = Client::new();
+            let resp = client.get(uri).await;
+            println!("RESP : {:?}", resp);
+        });
+    }
 
-    println!("3 Response : {}", resp.status());
+    // let uri = "http://naver.com".parse()?;
+    // let resp = client.get(uri).await?;
 
-    let uri = "http://google.com".parse()?;
-    let resp = client.get(uri).await?;
+    // println!("2 Response: {}", resp.status());
 
-    println!("4 Response : {}", resp.status());
+    // let uri = "http://daum.net".parse()?;
+    // let resp = client.get(uri).await?;
+
+    // println!("3 Response : {}", resp.status());
+
+    // let uri = "http://google.com".parse()?;
+    // let resp = client.get(uri).await?;
+
+    // println!("4 Response : {}", resp.status());
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod connector;
 
+use connector::Connector;
+
 fn main() {
     println!("Hello, world!");
 
-    let _ = connector::run();
-    let _ = connector::run1();
+    // let _ = connector::run();
+    // let _ = connector::run1();
+
+    let mut connector = Connector::new();
+    connector.push_uri("http://naver.com".parse().unwrap());
+    connector.push_uri("http://daum.net".parse().unwrap());
+    connector.push_uri("http://google.com".parse().unwrap());
+
+    let _ = connector.connect();
 }
