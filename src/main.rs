706f6e1ed62e75d8c798fd4cbab672d7fa9c2a15
Flame Allow <whynothoonhuh@gmail.com>
2020-07-27 23:49:41 +0900

    body stream example

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod craft;
 use connector::Connector;
 use craft::Craft;
 use hyper::{Body, Request};
+use hyper::body::HttpBody;
 use std::future::Future;
+use tokio::io::{self, AsyncWriteExt as _};
 
 fn main() {
     println!("Hello, world!");
@@ -12,10 +14,14 @@ fn main() {
     let _ = run();
 }
 
-async fn handle(index: usize, body: Body) {
-    let s = Craft::body_to_string(body).await.unwrap_or("".to_owned());
+async fn handle(index: usize, mut body: Body) {
+    // let s = Craft::body_to_string(body).await.unwrap_or("".to_owned());
 
-    println!("out : {}, {}", index, s.as_str());
+    // println!("out : {}, {}", index, s.as_str());
+    while let Some(next) = body.data().await {
+        let chunk = next.unwrap();
+        io::stdout().write_all(&chunk).await.unwrap();
+    }
 }
 
 #[tokio::main]
@@ -24,14 +30,14 @@ async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let req = Request::builder()
         .method("GET")
-        // .uri("https://cdn.hiyobi.me/data/json/1681767_list.json")
-        .uri("https://getmiso.com")
+        .uri("https://cdn.hiyobi.me/data/json/1681767_list.json")
+        // .uri("https://getmiso.com")
         .body(Body::empty())
         .unwrap();
 
     let reqs = vec![req];
 
-    craft.visit_all(reqs, Te::handle).await?;
+    craft.visit_all(reqs, handle).await?;
 
     Ok(())
 }
@@ -50,10 +56,10 @@ trait Handler {
     fn handle(index: usize, body: Body) -> Future<Output = ()>;
 }
 
-impl Handler for Te {
-    fn handle(index: usize, body: Body) -> Future<Output = ()> {
-        let s = Craft::body_to_string(body).await.unwrap_or("".to_owned());
+// impl Handler for Te {
+//     fn handle(index: usize, body: Body) -> Future<Output = ()> {
+//         let s = Craft::body_to_string(body).await.unwrap_or("".to_owned());
 
-        println!("Te::handle::out : {}, {}", index, s.as_str());
-    }
-}
+//         println!("Te::handle::out : {}, {}", index, s.as_str());
+//     }
+// }
